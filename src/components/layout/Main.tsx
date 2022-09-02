ab7a4365f403a0f82154907d0da1421f4b6c7685
benzotti <ben.zotti@paytron.com.au>
2022-09-02 12:00:01 +0930

    initial commit

---
+++ src/components/layout/Main.tsx
@@ -0,0 +1,24 @@
+import { memo, ReactNode } from "react";
+import Head from "next/head";
+
+interface PageProps {
+  children: ReactNode[] | ReactNode;
+}
+
+interface MainLayoutProps extends PageProps {
+  headerButtons?: JSX.Element[];
+}
+
+export const Main = ({ children, headerButtons }: MainLayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>BenZotti.me</title>
+        <meta name="description" content="Personal website for benzotti.me" />
+      </Head>
+      <main className="flex overflow-hidden flex-row h-screen w-full bg-black">
+        <div className="container mx-auto px-4">{children}</div>
+      </main>
+    </>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect, ReactElement } from "react";
+import type { NextPage } from "next";
+import { Main } from "../components/layout";
+
+export default function Home<NextPage>() {
+  return (
+    <span className="text-green-500 font-body">
+      Home page placeholder content
+    </span>
+  );
+}
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <Main>{page}</Main>;
+};
